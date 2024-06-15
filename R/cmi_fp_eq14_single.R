da932fb46597895c445925271b56b6c6544e363c
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2024-06-15 09:36:09 -0400

    Rename imputation_formula --> imputation_model and add AIC to return list

--- R/cmi_fp_eq14_single.R
+++ R/cmi_fp_eq14_single.R
@@ -2,27 +2,30 @@
 #'
 #' Single, fully parametric conditional mean imputation for a right-censored covariate using an accelerated failure-time model to estimate the conditional survival function and then integrates over the estimated survival function from \code{0} to \code{W} to compute conditional means using the stabilized integral.
 #'
-#' @param imputation_formula imputation model formula (or coercible to formula) passed through to \code{survreg}, a formula expression as for other regression models. The response is usually a survival object as returned by the \code{Surv} function. See \code{survreg} documentation for more details.
+#' @param imputation_model imputation model formula (or coercible to formula) passed through to \code{survreg}, a formula expression as for other regression models. The response is usually a survival object as returned by the \code{Surv} function. See \code{survreg} documentation for more details.
 #' @param dist imputation model distribution passed through to \code{survreg}. Options of \code{dist =} \code{"exponential"}, \code{"loglogistic"}, \code{"lognormal"}, and \code{"weibull"} currently accepted.
-#' @param W character, column name for observed values of the censored covariate
-#' @param Delta character, column name for censoring indicators. Note that \code{Delta = 0} is interpreted as a censored observation.
-#' @param data Dataframe or named matrix containing columns \code{W}, \code{Delta}, and any other variables in \code{imputation_formula}.
+#' @param data Dataframe or named matrix containing columns \code{W}, \code{Delta}, and any other variables in \code{imputation_model}.
 #' @param max_iter (optional) numeric, maximum iterations allowed in call to \code{survival::survreg()}. Default is \code{100}.
 #'
 #' @return
 #' \item{imputed_data}{A copy of \code{data} with added column \code{imp} containing the imputed values.}
 #' \item{code}{Indicator of algorithm status (\code{TRUE} or \code{FALSE}).}
+#' \item{aic}{Akaike information criterion (AIC) from the \code{imputation_model} fit.}
 #'
 #' @importFrom survival survreg
 #' @importFrom survival Surv
 #' @importFrom survival psurvreg
 
-cmi_fp_eq14_single = function(imputation_formula, dist, W, Delta, data, maxiter = 100) {
+cmi_fp_eq14_single = function(imputation_model, dist, data, maxiter = 100) {
   # Fit AFT imputation model for X ~ Z
-  fit = survreg(formula = imputation_formula,
+  fit = survreg(formula = imputation_model,
                 data = data,
                 dist = dist,
                 maxiter = maxiter)
+
+  # Extract variable names from imputation_model
+  W = all.vars(imputation_model)[1] ## censored covariate
+  Delta = all.vars(imputation_model)[2] ## corresponding event indicator
 
   # Initialize imputed values
   data$imp = data[, W] ## start with imp = W
@@ -132,6 +135,7 @@ cmi_fp_eq14_single = function(imputation_formula, dist, W, Delta, data, maxiter 
 
   # Return input dataset with appended column imp containing imputed values
   return_list = list(imputed_data = data,
-                     code = !any(is.na(data$imp)))
+                     code = !any(is.na(data$imp)),
+                     aic = AIC(object = fit))
   return(return_list)
 }
