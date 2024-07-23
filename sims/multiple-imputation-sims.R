d82a0ae680acae15efd46c4f0ca2b91d0232a3cb
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2024-07-23 13:43:06 -0400

    Rerun MI sims with heavier "heavy" censoring (.R, .csv)

--- sims/multiple-imputation-sims.R
+++ sims/multiple-imputation-sims.R
@@ -19,7 +19,7 @@ generate_data = function(n, censoring = "light") {
   q = ifelse(test = censoring == "light",
              yes = 0.2, # ~ 20%
              no = ifelse(test = censoring == "heavy",
-                         yes = 0.4, # ~35%
+                         yes = 0.7, # ~50%
                          no = 1.67) # ~79%
   ) # Rate parameter for censoring
   c = rexp(n = n, rate = q) # Random censoring mechanism
@@ -274,11 +274,8 @@ for (s in 1:nrow(sett_sp)) {
   if (sett_sp$num_imps[s] > 0) {
     ## Multiply impute censored covariates
     time_imp = system.time(
-      imp_fit <- cmi_sp_bootstrap(imputation_formula = Surv(time = w, event = d) ~ z,
-                                  analysis_formula = y ~ imp + z,
-                                  W = "w",
-                                  Delta = "d",
-                                  Z = "z",
+      imp_fit <- cmi_sp_bootstrap(imputation_model = Surv(time = w, event = d) ~ z,
+                                  analysis_model = y ~ imp + z,
                                   data = dat,
                                   trapezoidal_rule = TRUE,
                                   surv_between = "cf",
@@ -294,10 +291,7 @@ for (s in 1:nrow(sett_sp)) {
   } else {
     ## Impute censored covariates
     time_imp = system.time(
-      imp_dat <- cmi_sp(imputation_formula = Surv(time = w, event = d) ~ z,
-                        W = "w",
-                        Delta = "d",
-                        Z = "z",
+      imp_dat <- cmi_sp(imputation_model = Surv(time = w, event = d) ~ z,
                         data = dat,
                         trapezoidal_rule = TRUE,
                         surv_between = "cf",
@@ -313,6 +307,33 @@ for (s in 1:nrow(sett_sp)) {
     ## Save parameter estimates
     sett_sp[s, c(4:6)] = fit$coefficients
   }
+}
+
+## Full cohort
+sett_fc = expand.grid(sim = 1:sims,
+                      n = c(500, 1000, 2500, 5000),
+                      censoring = c("light", "heavy"),
+                      alpha_fc = NA,
+                      beta_fc = NA,
+                      gamma_fc = NA,
+                      time_fc = NA,
+                      se_alpha_fc = NA,
+                      se_beta_fc = NA,
+                      se_gamma_fc = NA)
+for (s in 1:nrow(sett_fc)) {
+  # Generate data
+  set.seed(sett_fc$sim[s]) ## set seed = sim ID
+  dat = generate_data(n = sett_fc$n[s], ## Sample size
+                      censoring = sett_fc$censoring[s]) ## Censoring setting
+
+  ## Fit model to full cohort data
+  fit = lm(y ~ x + z, data = dat)
+
+  ## Save parameter estimates
+  sett_fc[s, c(4:6)] = fit$coefficients
+
+  ## Save standard error estimates
+  sett_fc[s, c(8:10)] = sqrt(diag(vcov(fit)))
 }
 
 # /////////////////////////////////////////////////////////////////////////
@@ -338,7 +359,11 @@ sett = sett_old |>
                    by = dplyr::join_by(sim == sim,
                                        n == n,
                                        censoring == censoring,
-                                       num_imps == num_imps))
+                                       num_imps == num_imps)) |>
+  dplyr::left_join(sett_fc,
+                   by = dplyr::join_by(sim == sim,
+                                       n == n,
+                                       censoring == censoring))
 sett |>
   write.csv("speedyCMI/sims/multiple-imputation-sims.csv",
             row.names = F)
