f2982656bfe929d02edb28d1303d6b55bc05f6eb
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2024-08-13 13:59:07 -0400

    Convert data to dataframe (.R)

--- R/cmi_fp_bootstrap.R
+++ R/cmi_fp_bootstrap.R
@@ -7,8 +7,8 @@
 #' @param analysis_model Analysis model formula (or coercible to formula), a formula expression as for other regression models. The response should be a continuous outcome for normal linear regression.
 #' @param data Dataframe or named matrix containing columns \code{W}, \code{Delta}, and \code{Z}.
 #' @param type String name of the type of imputation procedure to be used. Options include \code{"original"} (the default), \code{"analytical"}, \code{"stabilized (with mean)"}, and \code{"stabilized (without mean)"}.
-#' @param B numeric, number of imputations. Default is \code{10}. 
-#' 
+#' @param B numeric, number of imputations. Default is \code{10}.
+#'
 #' @return A dataframe containing the pooled coefficients and standard errors for \code{analysis_model}
 
 #'
@@ -17,29 +17,32 @@
 cmi_fp_bootstrap = function(imputation_model, dist, analysis_model, data, type = "original", B = 10) {
   # Size of resample -----------------------------------------------------------
   n = nrow(data)
-  
+
+  # Make data a data frame (in case it's a tibble) -----------------------------
+  data = data.frame(data)
+
   # Extract variable names from imputation_model -------------------------------
   W = all.vars(imputation_model)[1] ## censored covariate
   Delta = all.vars(imputation_model)[2] ## corresponding event indicator
   Z = all.vars(imputation_model)[-c(1:2)] ## additional covariates
-  
-  # Take names and dimension from naive fit 
-  data[, "imp"] = data[, W] ## start imputed value with observed 
-  naive_fit = lm(formula = analysis_model, data = data) ## fit naive model 
-  p = length(naive_fit$coefficients) ## dimension of beta vector 
-  
-  # Initialize empty dataframe to hold results 
+
+  # Take names and dimension from naive fit
+  data[, "imp"] = data[, W] ## start imputed value with observed
+  naive_fit = lm(formula = analysis_model, data = data) ## fit naive model
+  p = length(naive_fit$coefficients) ## dimension of beta vector
+
+  # Initialize empty dataframe to hold results
   mult_fit = data.frame()
-  
-  # Loop through replicates 
+
+  # Loop through replicates
   for (b in 1:B) {
     # Sample with replacement from the original data ---------------------------
     re_rows = ceiling(runif(n = n, min = 0, max = 1) * nrow(data))
     re_data = data[re_rows, ]
-    
+
     # Calculate proportion of censored observations in resampled data ----------
     re_prop_cens = 1 - mean(re_data[, Delta])
-    
+
     # Check for censoring in resampled data
     if (0 < re_prop_cens) {
       # Impute censored x in re_data -------------------------------------------
@@ -69,38 +72,38 @@ cmi_fp_bootstrap = function(imputation_model, dist, analysis_model, data, type =
       }
 
       # After imputing, fit the analysis model ---------------------------------
-      re_fit = lm(formula = analysis_model, 
-                  data = re_data_imp$imputed_data) 
+      re_fit = lm(formula = analysis_model,
+                  data = re_data_imp$imputed_data)
     } else {
       # If no censored, just fit the usual model -------------------------------
       re_data$imp = re_data[, W]
-      re_fit = lm(formula = analysis_model, 
-                  data = re_data) 
+      re_fit = lm(formula = analysis_model,
+                  data = re_data)
     }
-    
+
     # Save coefficients to results matrix --------------------------------------
-    mult_fit = rbind(mult_fit, 
+    mult_fit = rbind(mult_fit,
                      summary(re_fit)$coefficients)
-  } 
-  
+  }
+
   ## Pool estimates
   pooled_est = rowsum(x = mult_fit[, 1],
                       group = rep(x = 1:p, times = B)) / B
-  
-  ## Calculate within-imputation variance 
+
+  ## Calculate within-imputation variance
   within_var = rowsum(x = mult_fit[, 2] ^ 2,
                       group = rep(x = 1:p, times = B)) / B
-  
+
   ## Calculate between-imputation variance
   between_var = rowsum(x = (mult_fit[, 1] - rep(pooled_est, times = B)) ^ 2,
                        group = rep(x = 1:p, times = B)) / (B - 1)
-  
+
   ## Pool variance
   pooled_var = within_var + between_var + (between_var / B)
-  
+
   # Return table of pooled estimates
   tab = data.frame(Coefficient = names(naive_fit$coefficients),
                    Est = pooled_est,
                    SE = sqrt(pooled_var))
-  return(tab)  
+  return(tab)
 }
