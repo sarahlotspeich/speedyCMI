76308708864f241c98e559f72e085983f95941bb
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2026-03-22 07:49:41 -0400

    Rerun SI sims

--- sims/single-imputation-sims.R
+++ sims/single-imputation-sims.R
@@ -205,7 +205,7 @@ for (s in 1:nrow(sett_sp)) {
   time_imp = system.time(
     imp_dat <- cmi_sp(imputation_model = Surv(time = w, event = d) ~ z,
                       data = dat,
-                      integral = "tr",
+                      trapezoidal_rule = TRUE,
                       surv_between = "cf",
                       surv_beyond = "d")
   )
